f823b292eea8ded21978c9357262fc37f8bb903f
juanmagape <juanmagape.trb@gmail.com>
2026-02-12 11:50:58 +0100

    Documents: Start docs idea- pending select by type and design

--- src/components/pages/documents.jsx
+++ src/components/pages/documents.jsx
@@ -1,11 +1,42 @@
-import { NavLink } from 'react'
+import { useState, useEffect } from 'react'
 
 function Documents() {
+  const [documentos, setDocumentos ] = useState([])
+
+  useEffect(() => {
+    async function fetchDocuments() {
+    try {
+      const res = await fetch('/documentos.json');
+      const data = await res.json();
+      setDocumentos(data);
+    } catch (error) {
+        console.error('Error fetching documents:', error) 
+      }
+    }
+    fetchDocuments();
+    },[])
+
 
   return (
     <>
       <h1>Documentos</h1>
 
+      <div>
+        {documentos.map((doc, index) => (
+          <div key={index}> 
+          <h2>{doc.tituloDoc}</h2> 
+          <p>{doc.tipoDoc}</p> 
+          <a href={doc.linkDoc} target="_blank" rel="noopener noreferrer">Ver documento</a> 
+          </div>))}
+      </div>
+
+      <div>
+        Nóminas
+      </div>
+
+      <div>
+        Otros
+      </div>
     </>
   )
 }
