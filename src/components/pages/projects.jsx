5455b0bd42567467a0f9ea807fe15be5d8557803
juanmagape <juanmagape.trb@gmail.com>
2026-02-18 11:15:32 +0100

    Projects: Add containers

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -2,54 +2,62 @@ import {useDraggable} from '@dnd-kit/react';
 import {DragDropProvider} from '@dnd-kit/react';
 import {useDroppable} from '@dnd-kit/react';
 import { useState } from 'react'
-
-
+import '../styles/projects.css'
 
 function Projects() {
-  const [isDropped, setIsDropped] = useState(false);
 
-  function Droppable({id, children}) {
-  const {ref} = useDroppable({
-    id,
-  })
-  return (
-          <div ref={ref} style={{width: 300, height: 300, border: "1px solid black"}}>
-            {children}
-          </div>
-  )
-};
+  const [parent, setParent] = useState(null);
+
+  function Droppable({ id, children }) {
+    const { ref } = useDroppable({ id });
+
+    return (
+      <div ref={ref} className="droppable">
+        {children}
+      </div>
+    );
+  }
 
   function Draggable() {
-  const {ref} = useDraggable({
-    id: 'draggable',
-  });
+    const { ref } = useDraggable({ id: "draggable" });
+
     return (
-    <a ref={ref}>
-      Draggable
-    </a>
-  );
-};
+      <div ref={ref} className="draggable">
+        Draggable
+      </div>
+    );
+  }
 
   return (
     <>
       <h1>Proyectos</h1>
 
-    <DragDropProvider
-      onDragEnd={(event) => {
-        if (event.canceled) return;
+      <DragDropProvider
+        onDragEnd={(event) => {
+          if (event.canceled) return;
 
-        const {target} = event.operation;
-        setIsDropped(target?.id === 'droppable');
-      }}
-    >
-      {!isDropped && <Draggable />}
+          const { target } = event.operation;
+          setParent(target?.id || null);
+        }}
+      >
 
-      <Droppable id="droppable">
-        {isDropped && <Draggable />}
-      </Droppable>
-    </DragDropProvider>
+        {parent === null && <Draggable />}
+
+        <Droppable id="todo">
+          {parent === "todo" && <Draggable />}
+        </Droppable>
+
+        <Droppable id="in-progress">
+          {parent === "in-progress" && <Draggable />}
+        </Droppable>
+
+        <Droppable id="done">
+          {parent === "done" && <Draggable />}
+        </Droppable>
+
+      </DragDropProvider>
     </>
-  )
+  );
 }
 
-export default Projects;
+export default Projects
