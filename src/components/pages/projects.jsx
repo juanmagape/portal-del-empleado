5922356e6aa7f7235049b637deb0f3a8e526511a
juanmagape <juanmagape.trb@gmail.com>
2026-02-19 23:02:49 +0100

    Projects: Update design

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -3,25 +3,37 @@ import {DragDropProvider} from '@dnd-kit/react';
 import {useDroppable} from '@dnd-kit/react';
 import { useState, useEffect } from 'react'
 import '../styles/projects.css'
+import '../../App.css'
 
-const columns = ["todo", "in-progress", "done"];
+const columns = [
+  {
+    id: "todo", 
+    label: "TO DO" 
+  }, {
+    id:"in-progress", 
+    label: "EN PROGRESO"
+  }, 
+  {
+    id: "done", 
+    label: "HECHO"
+  }];
 
 function Draggable({ id, title }) {
-  const { ref } = useDraggable({ id });
+  const { ref, isDragging } = useDraggable({ id });
 
   return (
-    <div ref={ref} className="draggable">
+    <div ref={ref} className={`task draggable ${isDragging ? 'task--dragging' : ''}`}>
       {title}
     </div>
   );
 }
 
-function Droppable({ id, children }) {
+function Droppable({ id, children, label }) {
   const { ref } = useDroppable({ id });
 
   return (
     <div ref={ref} className="droppable">
-      <h3>{id}</h3>
+      <h3 className='tituloDropp'>{label}</h3>
       {children}
     </div>
   );
@@ -55,7 +67,7 @@ const [error, setError] = useState(null);
 
 return (
   <>
-    <h1>Proyectos</h1>
+    <h1 className='title'>Proyectos</h1>
     <div className="containers">
       <DragDropProvider
         onDragEnd={(event) => {
@@ -71,9 +83,9 @@ return (
         }}
       >
         {columns.map((col) => (
-          <Droppable key={col} id={col}>
+          <Droppable key={col.id} id={col.id} label={col.label}>
             {items
-              .filter((item) => item.column === col)
+              .filter((item) => item.column === col.id)
               .map((item) => (
                 <Draggable key={item.id} id={item.id} title={item.title} />
               ))}
