49f06c445a6ef167848ecb72934eb03030178243
juanmagape <juanmagape.trb@gmail.com>
2026-02-22 18:51:53 +0100

    Surveys: Update content + change design + add animation

--- src/components/pages/surveys.jsx
+++ src/components/pages/surveys.jsx
@@ -1,59 +1,82 @@
+import { useRef } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import '../styles/surveys.css'
 
 function Surveys() {
+  const modalRef = useRef(null);
+  const overlayRef = useRef(null);
+
+  const handleClose = (close) => {
+    const popupContent = modalRef.current?.closest('.popup-content');
+    const popupOverlay = modalRef.current?.closest('.popup-overlay');
+
+    if (popupContent) popupContent.classList.add('exiting');
+    if (popupOverlay) popupOverlay.classList.add('exiting');
+
+    setTimeout(() => {
+      close();
+    }, 700);
+  };
 
   return (
     <>
       <h1>Encuestas</h1>
-  <Popup
-    trigger={<button className="button"> Open Modal </button>}
-    modal
-    nested
-  >
-    {close => (
-      <div className="modal">
-        <h1>Encuesta</h1>
-        
-        <h3>Pregunta 1</h3>
-        <label>
-          <input type="checkbox" id="cbox1" value="first_checkbox" /> 
-          Este es mi primer checkbox
-          <input type="checkbox" id="cbox1" value="first_checkbox" /> 
-          Este es mi primer checkbox
-          <input type="checkbox" id="cbox1" value="first_checkbox" /> 
-          Este es mi primer checkbox
-          <input type="textarea" id="cbox1" placeholder='Ejemplo 1' /> 
-          
-        </label>
+      <Popup
+        trigger={<button className="button"> Open Modal </button>}
+        modal
+        nested
+        contentStyle={{
+          borderRadius: "20px",
+          padding: "20px"
+        }}
+      >
+        {close => (
+          <>
+            <div
+              ref={overlayRef}
+              className="overlay-anim"
+            />
+            <div ref={modalRef} className="modal">
+              <h1>Encuesta</h1>
 
-        <div className="content">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-          Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-          delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-          commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
-          explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
-        </div>
-        <div className="actions">
+              <h3>Pregunta 1</h3>
+              <div className='quest'>
+                <label>
+                  <input type="checkbox" id="cbox1" value="first_checkbox" />
+                  <p>A</p>
+                </label>
+                <label>
+                  <input type="checkbox" id="cbox2" value="first_checkbox" />
+                  <p>B</p>
+                </label>
+                <label>
+                  <input type="checkbox" id="cbox3" value="first_checkbox" />
+                  <p>C</p>
+                </label>
+              </div>
 
-          <button
-            className="button"
-            onClick={() => {
-              console.log('modal closed ');
-              close();
-            }}
-          >
-            Cerrar encuesta
-          </button>
-        </div>
-      </div>
-    )}
-  </Popup>
+              <label>
+                <input type="textarea" id="text" placeholder='Ejemplo 1' />
+              </label>
+
+              <div className="actions">
+                <button
+                  className="button"
+                  onClick={() => {
+                    console.log('modal closed');
+                    handleClose(close);
+                  }}
+                >
+                  Cerrar encuesta
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+      </Popup>
     </>
-  )
+  );
 }
 
 export default Surveys;
